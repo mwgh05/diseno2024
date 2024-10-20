f512c66a7886bb5b080cc9d60807862929aff8d0
mora-urb <andresmora1914@gmail.com>
2024-10-20 04:12:24 -0600

    Update material.py

--- Proyecto/lab-doc/src/boilerplate/models/material.py
+++ Proyecto/lab-doc/src/boilerplate/models/material.py
@@ -1,1 +1,33 @@
+class Material:
+    def __init__(self, id, name, description):
+        self.id = id
+        self.name = name
+        self.description = description
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description
+        }
+
+    @staticmethod
+    def from_json(json_data):
+        return Material(
+            json_data['id'],
+            json_data['name'],
+            json_data['description']
+        )
+
+    def get_name(self):
+        return self.name
+
+    def get_description(self):
+        return self.description
+
+    def set_name(self, name):
+        self.name = name
+
+    def set_description(self, description):
+        self.description = description
 
